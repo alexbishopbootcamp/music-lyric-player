f0002f4810cd8ae779fc558ec3a244e28f690f13
Alex <github@alexbi.shop>
2023-07-25 07:17:44 +0900

    Populated track info, improved track search

--- js/app.js
+++ js/app.js
@@ -41,18 +41,61 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 async function listTracks(query) {
   const tracks = await spotifySearchTracks(query);
-  console.log(tracks);
   for (let track of tracks.tracks.items) {
     const trackDiv = document.createElement('div');
+    trackDiv.classList.add('track-search-result');
     trackDiv.textContent = track.name;
     trackDiv.style.border = '1px solid black';
     trackDiv.style.padding = '2px';
     trackDiv.style.margin = '2px';
+    trackDiv.dataset.uri = track.uri;
     trackDiv.addEventListener('click', () => {
-      loadUri(track.uri);
+      onTrackSearchResultClick(trackDiv);
     });
-    document.body.appendChild(trackDiv);
+    document.querySelector('#search-results').appendChild(trackDiv);
   }
+}
+
+
+function onTrackSearchResultClick(button){
+  // Get track URI from button and load it
+  const trackUri = button.dataset.uri;
+  // Load track into player
+  loadUri(trackUri);
+  // Populate track information
+  populateTrackInformation(trackUri);
+  // Remove all track search results
+  const trackSearchResults = document.querySelectorAll('.track-search-result');
+  for (let trackSearchResult of trackSearchResults) {
+    trackSearchResult.remove();
+  }
+  // Switch to main page
+  showMainPage();
+}
+
+async function populateTrackInformation(trackUri){
+  // Get track information
+  const track = await spotifyGetTrack(trackUri);
+  // Populate track information
+  document.querySelector('#album-art').src = track.album.images[0].url;
+  document.querySelector('#track-title').textContent = track.name;
+  document.querySelector('#artist-name').textContent = track.artists[0].name;
+  document.querySelector('#album-name').textContent = track.album.name;
+  document.querySelector('#release-year').textContent = track.album.release_date.slice(0, 4);
+  document.querySelector('#song-length').textContent = msToTime(track.duration_ms);
+}
+
+// Convert MS into a track duration
+function msToTime(duration) {
+  // Calculate seconds, minutes and hours and add leading zeroes
+  const seconds = parseInt((duration / 1000) % 60);
+  secondsStr = seconds < 10 ? `0${seconds}` : seconds;
+  const minutes = parseInt((duration / (1000 * 60)) % 60);
+  minutesStr = minutes < 10 ? `0${minutes}` : minutes;
+  const hours = parseInt((duration / (1000 * 60 * 60)));
+  // Omit hours if song is less than an hour
+  const hoursStr = hours > 0 ? hours + ":" : "";
+  return `${hoursStr}${minutesStr}:${secondsStr}`;
 }
 
 function showMainPage() {
