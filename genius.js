9656407088f9f4ea909d49004db17edab8be5c24
eliabn <30064396@tafe.wa.edu.au>
2023-07-25 15:29:02 +0800

    Complete getId and getLyric fetch fuctions

--- genius.js
+++ genius.js
@@ -3,25 +3,68 @@ var secrets = {};
 var mainText = document.getElementById('text')
 var mainText = document.getElementById('text')
 
+// fetch('secrets.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     secrets = data;
+//     console.log(secrets);
+//   }
+// );
 
-fetch('secrets.json')
-  .then(response => response.json())
+secrets = 'access_token=B98dceKwMBqBfHmExWu8E3xQ4SB-m8b4OGKlxj4Xc4elRZ6oQUhE-HfjpXpDMxLp'
+
+var toSearch = 'all of me'
+
+
+
+const urlQuery = encodeURIComponent(toSearch);
+
+console.log(urlQuery)
+
+function getSongDetails() {
+  const requestUrl = 'https://api.genius.com/search?q=' + urlQuery + '&' + secrets;;
+  
+  const sondId = fetch(requestUrl, {
+    method: 'GET',
+  })
+  .then(response => {
+    return response.json();
+  })
   .then(data => {
-    secrets = data;
-    console.log(secrets);
-  }
-);
+    var song = data.response.hits[0].result
+    var songId = '200930'
+    //song.id.toString()
+    var title = song.full_title
+    var lCaseTitle = title.toLowerCase()
+    var splitlCaseTitle = lCaseTitle.split(' by')[0]
+    var lCasetoSearch = toSearch.toLowerCase()
+    console.log(splitlCaseTitle);
 
-secrets = 'aGYq4jMa_2pNIMUhl6nXBIg7os8XHnPkIa8Hw36CU968w8kJUUBH4hcJE6ZJx3Le'
-function getApi() {
+    if (splitlCaseTitle === lCasetoSearch) {
+      getSongLyrics(songId)
+      console.log(songId)
+    } else {
+      console.log("not same")
+    }
+  })
+}
+getSongDetails()
+
+
+
+
+
+function getSongLyrics(songId) {
   // fetch request gets a list of all the repos for the node.js organization
-
-const requestUrl = 'https://api.genius.com/songs/378195?access_token='+secrets;
+var id = songId
+console.log(id)
+const requestUrl = 'https://api.genius.com/songs/'+ songId + '?' + secrets;
+console.log(requestUrl)
 const lyric = fetch(requestUrl, {
     method: 'GET',
     })
     .then(response => {
-        console.log(secrets)
+      console.log(secrets)
       console.log(response)
       return response.json();
     })
@@ -31,18 +74,20 @@ const lyric = fetch(requestUrl, {
 
       var song = data.response.song.embed_content;
 
-      //var link = document.createElement('div');
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(song, "text/html");
 
-      //link.innerHTML = song
+      let scriptTag = doc.body.childNodes[2];
+      console.log(scriptTag.src);
 
-      //console.log(song)
-      console.log(mainText)
-
-      //mainText.appendChild(link);
-      
-      //mainText.innerHTML = hhh "n/" +  song 
+      var src = scriptTag.src
 
       console.log(mainText)
+
+      console.log(mainText)
+      const requestUrl = src;
+
+      displayLyrics(requestUrl)
       return song;
 
     })
@@ -54,31 +99,31 @@ const lyric = fetch(requestUrl, {
     console.log(lyric)
     return lyric;
 }
-var gg = getApi();
-getApi()
+//var gg = getApi();
+//getApi()
 
-console.log(gg)
+//console.log(gg)
 
 
-const requestUrl = 'https://genius.com/songs/2948513/embed.js';
-
-const lyric = fetch(requestUrl, {
+function displayLyrics(requestUrl) {
+  console.log(requestUrl)
+  const lyric = fetch(requestUrl, {
     method: 'GET',
     })
     .then(response => {
       return response.text();
     })
-
-async function f1() {
-  const lyricText = await lyric;
-//console.log(lyricText)
-const regex = /(JSON\.parse.+)./g;
-const match = regex.exec(lyricText)[0].split(0, -1);
-const myScript = document.createElement('script');
-myScript.innerHTML += 'document.querySelector("#text").innerHTML = (';
-myScript.innerHTML += match;
-console.log(myScript);
-document.body.appendChild(myScript);
+  async function f1() {
+    const lyricText = await lyric;
+    console.log(lyricText)
+    const regex = /(JSON\.parse.+)./g;
+    const match = regex.exec(lyricText)[0].split(0, -1);
+    const myScript = document.createElement('script');
+    myScript.innerHTML += 'document.querySelector("#text").innerHTML = (';
+    myScript.innerHTML += match;
+    console.log(myScript.innerHTML);
+    document.body.appendChild(myScript);
+  }
+  f1()
 }
 
-f1()
